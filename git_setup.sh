0693d7572e60dd828d6d067fa5684463d7c7dc6a
Nandakishore Saboo <nandakishoresaboo@gmail.com>
2017-11-04 02:55:55 +0000

    adds git_setup.sh

---
+++ git_setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+source /etc/bash_completion.d/docker-compose 
+
+git clone https://github.com/lena-team/lena-consumer.git
+cd lena-consumer
+git config --global user.name 'Nandakishore Saboo'
+git config --global user.email 'nandakishoresaboo@gmail.com'
