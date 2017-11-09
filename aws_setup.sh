181dc6fd89f41bf6ed9248a1a26b337a871ef0ef
Nandakishore Saboo <nandakishoresaboo@gmail.com>
2017-11-08 19:03:48 -0800

    - updates logstash configuration

--- aws_setup.sh
+++ aws_setup.sh
@@ -33,3 +33,6 @@ source /etc/bash_completion
 # git clone https://github.com/lena-team/lena-consumer.git
 
 # docker swarm related ports need to be added to security groups and attached to ec2 instances micros
+
+# for nvm if required
+# curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.6/install.sh | bash && source ~/.bashrc && nvm install 8.9.1
